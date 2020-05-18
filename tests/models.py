3eee766a37eb7b0fe56408edffbae7d017a83fdb
Indra Rudianto <indrarudianto.official@gmail.com>
2020-05-18 08:31:20 +0700

    Add DataModel test unit

---
+++ tests/models.py
@@ -0,0 +1,36 @@
+import unittest
+
+from bmaclient.models import DataModel
+
+
+class DataModelTest(unittest.TestCase):
+    def test_empty_keys(self):
+        model = DataModel()
+        self.assertListEqual(model.get_keys(), list())
+        self.assertDictEqual(model.as_dict(), dict())
+
+    def test_setter(self):
+        model = DataModel()
+        model['key1'] = 'value1'
+        model['key2'] = 'value2'
+        self.assertListEqual(model.get_keys(), ['key1', 'key2'])
+        self.assertDictEqual(
+            model.as_dict(), {'key1': 'value1', 'key2': 'value2'})
+
+    def test_getter(self):
+        model = DataModel()
+        model['key1'] = 'value1'
+        model['key2'] = 'value2'
+        self.assertEqual(model['key1'], 'value1')
+        self.assertEqual(model['key2'], 'value2')
+
+    def test_object_from_dict(self):
+        dict_data = {'key1': 'value1', 'key2': 'value2'}
+        model = DataModel(**dict_data)
+        self.assertListEqual(model.get_keys(), ['key1', 'key2'])
+        self.assertDictEqual(
+            model.as_dict(), {'key1': 'value1', 'key2': 'value2'})
+
+
+if __name__ == '__main__':
+    unittest.main()
