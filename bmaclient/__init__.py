56aca3f650d17c8a99e0fb63d850f5d716caae7a
Indra Rudianto <indrarudianto.official@gmail.com>
2020-11-07 08:56:57 +0700

    Import parameter encoder class to top module

--- bmaclient/__init__.py
+++ bmaclient/__init__.py
@@ -1,4 +1,5 @@
 from .bind import APIClientError, APIError, MonitoringAPIMethod
 from .client import MonitoringAPI
+from .encoder import ParameterEncoder
 from .models import ApiModel, DataModel
 from .version import __version__
